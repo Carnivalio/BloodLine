99f86d6bd31e646e3cd00757db0249e3b73a0d8a
Carnivalio <blank.riza@gmail.com>
2017-10-09 05:17:56 +1100

    Add files via upload

---
+++ Automatic Dependency Innitialization.bat
@@ -0,0 +1,9 @@
+pip install Django
+pip install djangorestframework
+pip install python-social-auth[django]
+pip install django-bootstrap3
+pip install django-simple-captcha
+pip install django-jinja
+python manage.py makemigrations bloodline
+python manage.py migrate
+python manage.py runserver
